77711a1e6ce3c6fb589d6a4ead9424c103b930d9
slayerzeroa <slayerzeroa@naver.com>
2022-07-25 21:27:25 +0900

    mbtiguru_220725_12

--- py/guru_portfolio.py
+++ py/guru_portfolio.py
@@ -2,20 +2,19 @@
 import sqlite3
 import pandas as pd
 
-berkshire = pd.read_csv("berkshire_hathaway_inc.csv")
 
-# def get_portfolio():        # 포트폴리오 받아오는 함수
-#     con = sqlite3.connect("./db/guru.db")   # db 접근
-#     cur = con.cursor()                      # 커서 생성
-#     query = cur.execute("SELECT * FROM berkshire")  # 쿼리 설정
-#     cols = [column[0] for column in query.description]  # 컬럼 설정
-#     portfolio = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)      # pandas 포트폴리오 가져오기
-#     con.close()     # sqlite 종료
-#     return portfolio    # 포트폴리오 리턴
+def get_portfolio():        # 포트폴리오 받아오는 함수
+    con = sqlite3.connect("./db/guru.db")   # db 접근
+    cur = con.cursor()                      # 커서 생성
+    query = cur.execute("SELECT * FROM berkshire")  # 쿼리 설정
+    cols = [column[0] for column in query.description]  # 컬럼 설정
+    portfolio = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)      # pandas 포트폴리오 가져오기
+    con.close()     # sqlite 종료
+    return portfolio    # 포트폴리오 리턴
 
 def cut(df):
     df.columns = df.columns.str.replace(' ', '_')
-    del df['Type']
+    cut_df = df.dropna(axis = 1, how = 'all')
+    return cut_df
 
-def print_portfolio():
-    return(cut(get_portfolio()))
+print(cut(get_portfolio()))
