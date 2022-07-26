339be13bd757e85796ccc1b2d5e78252d44533db
slayerzeroa <slayerzeroa@naver.com>
2022-07-26 19:45:57 +0900

    mbtiguru_220726_37

--- js/data.js
+++ js/data.js
@@ -7,7 +7,7 @@ const select = [];
 
 function go_getresult(){
   let point = calculate();
-  export default const result1 = infoList[point].name;
+  export default result1 = infoList[point].name;
 }
 
 function goguru(){
